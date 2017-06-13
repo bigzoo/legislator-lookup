662ecf63d20f15af41ad80a2858d56dc6b6f7230
Chris Nyaga <nyagachris75@gmail.com>
2017-06-13 11:20:52 +0300

    API request for data added to results route

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function (params) {
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=0613f3c5dde44d699a1a8c7adb2e6ed7&zip=' +
+    params.zip;
+    return  Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
 });
