915b7b59060aa62f1f2977e67960237e1058d572
Chris Nyaga <nyagachris75@gmail.com>
2017-06-13 14:27:41 +0300

    Private Api key now used in the API request

--- app/routes/results.js
+++ app/routes/results.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=0613f3c5dde44d699a1a8c7adb2e6ed7&zip=' +
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key'&zip=' +
     params.zip;
     return  Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
