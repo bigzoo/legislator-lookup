bae62faf3d31bc754e0c9c7ed96f42fdf8e2e820
Chris Nyaga <nyagachris75@gmail.com>
2017-06-13 14:29:31 +0300

    Bug fixes

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,8 +4,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function (params) {
     var key = config.myApiKey;
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key'&zip=' +
-    params.zip;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip='+params.zip;
     return  Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
